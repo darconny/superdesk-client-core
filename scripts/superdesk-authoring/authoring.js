579a99d6ee5e8d280d65c59cf7c61452761f18c3
darconny <darko.tomic@gmail.com>
2016-02-29 15:57:57 +0100

    Image crop modal - UI work

--- scripts/superdesk-authoring/authoring.js
+++ scripts/superdesk-authoring/authoring.js
@@ -2474,7 +2474,7 @@
                 .activity('edit.crop', {
                     label: gettext('EDIT CROP'),
                     modal: true,
-                    cssClass: 'upload-avatar modal-static modal-responsive',
+                    cssClass: 'edit-image modal-responsive',
                     controller: ChangeImageController,
                     templateUrl: 'scripts/superdesk-authoring/views/change-image.html',
                     filters: [{action: 'edit', type: 'crop'}]
